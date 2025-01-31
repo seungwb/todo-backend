4fd9717bad6d0ec3aa298be63df57c95f888914c
seungwb <seungwb@gmail.com>
2025-01-31 17:46:27 +0900

    feat: Spring Security 설정
    [jjwt 0.12.6 버전 적용
    인증 filter 적용]

---
+++ src/main/java/kr/co/tododeungjang/web/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package kr.co.tododeungjang.web.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.tododeungjang.web.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.io.IOException;
+
+@Configurable
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/api/v1/auth/**", "/api/v1/search/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/board/**").permitAll()
+                .anyRequest().authenticated()
+                ).exceptionHandling(handler -> handler.authenticationEntryPoint(new FailedAuthenticationEntryPoint()));
+
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+}
+
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("{\"FOBIDDEN ERROR\"}");
+    }
+}
---
+++ src/main/java/kr/co/tododeungjang/web/provider/JwtProvider.java
@@ -0,0 +1,54 @@
+package kr.co.tododeungjang.web.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+
+@Component
+public class JwtProvider {
+
+    private SecretKey secretKey = Jwts.SIG.HS256.key().build();
+
+    //JWT 생성 메서드
+    public String create(String email){
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS)); //현재시간에서 1시간 추가
+
+        String jwt = Jwts.builder()
+                .signWith(secretKey)        //암호화 알고리즘
+                .subject(email)             //사용자 식별자 값
+                .issuedAt(new Date())       //생성 시간
+                .expiration(expiredDate)    //만료 시간
+                .compact();                 //압축하여 객체 생성
+
+        return jwt;
+    }
+
+    //JWT 검증 메서드
+    public String validate(String jwt){
+        Claims claims = null;
+
+        try{
+            claims = Jwts.parser().verifyWith(secretKey)    //서명 검즘
+                    .build()                                //빌드
+                    .parseSignedClaims(jwt)                 //JWT 파싱
+                    .getPayload();                          //Claims 추출
+
+        } catch (JwtException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return claims.getSubject();
+    }
+}
